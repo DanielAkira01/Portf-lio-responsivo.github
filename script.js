5745a82ef96eea4629a13169db841ea620de0e2a
Daniel Akira <146569618+DanielAkira01@users.noreply.github.com>
2023-10-28 20:47:35 -0300

    responsive

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ mobilemenu.addEventListener("click", function() {
     mobilemenu.classList.toggle('active');
     menushow.classList.toggle('active');
 });
+
+// Adicione o seguinte código para tornar o menu responsivo
+
+window.addEventListener("resize", function() {
+    if (window.innerWidth > 768) { // Ajuste o valor conforme necessário
+        mobilemenu.classList.remove('active');
+        menushow.classList.remove('active');
+    }
+});
