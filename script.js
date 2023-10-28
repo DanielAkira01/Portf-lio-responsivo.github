c01f04425fc58f1a52eef47718c3fb6301ba7d53
Daniel Akira <146569618+DanielAkira01@users.noreply.github.com>
2023-10-28 13:45:13 -0300

    resetcode

--- script.js
+++
@@ -1,7 +0,0 @@
-const mobilemenu = document.querySelector(".mobile-menu")
-const icon = document.querySelector(".list")
-
-mobilemenu.addEventListener("click",() =>{
-    mobilemenu.classList.toggle('active');
-    icon.classList.toggle('active');
-})
