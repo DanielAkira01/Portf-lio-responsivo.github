37c1c478c583951209b32b14a46cc5822eb1b50e
Daniel Akira <146569618+DanielAkira01@users.noreply.github.com>
2023-10-28 13:53:02 -0300

    mobile menu

---
+++ script.js
@@ -0,0 +1,8 @@
+const mobilemenu = document.querySelector("mobilemenu")
+const menuicon = document.querySelector("navigation_header")
+
+
+mobilemenu.addEventListener("clik", ()=>){
+    mobilemenu.classList.toggle('active')
+    menuicon.classList.toggle('active')
+}
