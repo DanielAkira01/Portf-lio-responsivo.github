3c20f297691f61ef37e4d4efeabbcd9bf2a621d2
Daniel Akira <146569618+DanielAkira01@users.noreply.github.com>
2023-10-28 14:07:21 -0300

    resetcode

--- script.js
+++
@@ -1,8 +0,0 @@
-const mobilemenu = document.querySelector("mobilemenu");
-const item = document.querySelector("navigation_header");
-
-
-mobilemenu.addEventListener("clik", ()=>{
-    mobilemenu.classList.toggle('active');
-    item.classList.toggle('active');
-})
