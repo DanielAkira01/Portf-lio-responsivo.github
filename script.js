96df45f0f4f3e45a2ea80499c874a38b6767775d
Daniel Akira <146569618+DanielAkira01@users.noreply.github.com>
2023-10-28 13:58:17 -0300

    correção

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const mobilemenu = document.querySelector("mobilemenu")
-const menuicon = document.querySelector("navigation_header")
+const mobilemenu = document.querySelector("mobilemenu");
+const menuicon = document.querySelector("navigation_header");
 
 
-mobilemenu.addEventListener("clik", ()=>){
-    mobilemenu.classList.toggle('active')
-    menuicon.classList.toggle('active')
-}
+mobilemenu.addEventListener("clik", ()=>{
+    mobilemenu.classList.toggle('active');
+    menuicon.classList.toggle('active');
+})
