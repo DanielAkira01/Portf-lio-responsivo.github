72bff8a7185c75e885da6675ebe6ffcc85ceab91
Daniel Akira <146569618+DanielAkira01@users.noreply.github.com>
2023-10-28 14:57:51 -0300

    update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const mobilemenu = document.querySelector(".mobilemenu");
-const menushow = document.querySelector(".navigation_header");
+const menushow = document.querySelector(".nav-menu");
 
-mobilemenu.addEventListener("click", () => {
-    mobilemenu.classList.toggle('active');
-    menushow.classList.toggle('active');
-})
+mobilemenu.addEventListener("click", () {
+        mobilemenu.classList.toggle('active');
+        menushow.classList.toggle('active');
+    })
